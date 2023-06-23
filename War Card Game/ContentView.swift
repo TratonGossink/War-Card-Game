a9232af08f8164ccbeacccc2730a5b4897e99be7
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-06-22 19:03:12 -0500

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Traton Gossink on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
