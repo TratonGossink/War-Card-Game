cc41d816cda00c9320f2808f4493b500439942a3
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-07-15 08:03:25 -0500

    Initial Commit

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,19 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var plyrCard = "card4"
+    @State var cpuCard = "card8"
+    @State var plyrScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Image("background-plain")
+                .ignoresSafeArea()
+            VStack(alignment: .center
+                   , spacing: 30) {
+                Image("logo")
+                    .padding()
+                    .padding(.bottom)
+                HStack {
+                    Image(cpuCard)
+                        .padding(25)
+                    Image(plyrCard)
+                        .padding()
+                }
+                .padding(.bottom)
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                HStack(alignment: .center, spacing: 120) {
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                    }
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(plyrScore))
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                    }
+                }
+                .foregroundColor(.white)
+            }
+                   .offset(y: -30)
+                   .padding()
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func deal() {
+        
+        var plyrValue = Int.random(in: 2...14)
+        var cpuValue = Int.random(in: 2...14)
+        
+        plyrCard = "card" + String(plyrValue)
+        
+        cpuCard = "card" + String(cpuValue)
+        
+        if plyrValue > cpuValue {
+            plyrScore += 1
+            
+        } else {
+            cpuScore += 1
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
